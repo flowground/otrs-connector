11219b77a42bf8d881500e9ecc37ce7f290f2b80
vlionescu <vladm.ionescu@endava.com>
2018-10-08 21:34:51 +0300

    initial commit

---
+++ verifyCredentials.js
@@ -0,0 +1,25 @@
+"use strict";
+module.exports = verify;
+const OtrsConnector = require('./lib/services/OtrsConnector');
+
+/**
+ * Executes the verification logic by sending a simple to the Petstore API using the provided apiKey.
+ * If the request succeeds, we can assume that the apiKey is valid. Otherwise it is not valid.
+ *
+ * @param credentials object
+ *
+ * @returns
+ */
+function verify(credentials) {
+	const { user, password } = credentials;
+
+	const otrs = new OtrsConnector('https://rrsg.managed-otrs.com/otrs/nph-genericinterface.pl/Webservice/ws1', {user , password});
+
+	otrs.getTickets({
+		"TicketNumber": "0"
+	}).then(
+		() => console.log('success')
+	).catch(
+		() => console.log('err')
+	);
+}
